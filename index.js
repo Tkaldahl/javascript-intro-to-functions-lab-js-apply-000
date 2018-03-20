854f57bf135c5ba7c8f9d2469d8dc91df4ccd588
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-03-20 01:22:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if ('hello') console.log('I can\'t hear you!')
-
   if ('HELLO') console.log('YES INDEED!')
-
   if ('I love you, Grandma.') console.log('I love you, too.')
 }
 
